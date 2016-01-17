4664fbb3d27f543fee1a9c12732dc44f29a02524
Jens Wittmann <jenswittmann@users.noreply.github.com>
2016-01-17 23:45:10 +0100

    adjustments for all-inkl.com
    To use it on a all-inkl.com Shared-/Root-Host you have to activate "/bin/pktar" as alias for "tar" (line 88/93) over the support.

--- core/components/backupmodx/elements/snippets/backupmodxwidget.snippet.php
+++ core/components/backupmodx/elements/snippets/backupmodxwidget.snippet.php
@@ -85,13 +85,13 @@ if (isset($_POST['backupMODX'])) {
 		
 		//File-Backup
 		if (!empty($_POST["files"])){
-			system("tar cf {$targetTar} --exclude=$dir $base_path $core_path");
+			system("/bin/pktar cf {$targetTar} --exclude=$dir $base_path $core_path");
 		}
 		
 		//Combine SQL and Files in one archive
 		if (file_exists($targetSql) and file_exists($targetTar) and filesize($targetSql) > 0) {
 			system("cp {$targetTar} {$targetCom}"); //copy files-archive
-			system("tar uf {$targetCom} -C $dir {$dbase}_{$date}_mysql.sql"); //adding sql-file in the root
+			system("/bin/pktar uf {$targetCom} -C $dir {$dbase}_{$date}_mysql.sql"); //adding sql-file in the root
 		}
 		
 		$backup = true;
